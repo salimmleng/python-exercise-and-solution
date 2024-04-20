22faf269c9245afb3eaa92ee5f706e291ed96d46
Salim Hossain <167601764+salimmleng@users.noreply.github.com>
2024-04-19 23:27:03 -0700

    Add files via upload

---
+++ practise1/reverse_words.py
@@ -0,0 +1,14 @@
+# S = input().split()
+# reversed_words = [S[::-1] for S in S]
+# reversed_string =" ".join(reversed_words)
+# print(reversed_string)
+
+
+# name,age = input().split()
+
+# print(name,age)
+
+
+
+a,b = map(int,input().split())
+print(a+b)
